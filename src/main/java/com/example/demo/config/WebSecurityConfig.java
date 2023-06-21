b194f2c435d978a1f2ef903a46ca43bb5f883155
shunya <fjfj_07fabric@outlook.jp>
2023-06-22 05:26:34 +0900

    [update] part21時点ソース
    Spring Securityを使ってログイン処理を実装する！(④デフォルトエラーメッセージの変更)

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,12 +1,19 @@
 package com.example.demo.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.demo.constant.UrlConst;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Spring Securityカスタマイズ用
@@ -16,7 +23,17 @@ import com.example.demo.constant.UrlConst;
  */
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+	/** パスワードエンコーダー */
+	private final PasswordEncoder passwordEncoder;
+
+	/** ユーザー情報取得Service */
+	private final UserDetailsService userDetailsService;
+
+	/** メッセージ取得 */
+	private final MessageSource messageSource;
 
 	/** ユーザー名のname属性 */
 	private final String USERNAME_PARAMETER = "loginId";
@@ -41,5 +58,20 @@ public class WebSecurityConfig {
 								.defaultSuccessUrl(UrlConst.MENU)); // ログイン成功後のリダイレクトURL
 
 		return http.build();
+	}
+
+	/**
+	 * Provider定義
+	 * 
+	 * @return カスタマイズProvider情報
+	 */
+	@Bean
+	AuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		provider.setMessageSource(messageSource);
+
+		return provider;
 	}
 }
