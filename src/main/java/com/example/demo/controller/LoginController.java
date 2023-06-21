d5dc6b36ecebef7989e27bae2c7f9e17d1299b4e
shunya <fjfj_07fabric@outlook.jp>
2023-06-22 05:27:53 +0900

    [fix] 未使用ソース、定義を削除

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.context.MessageSource;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.constant.UrlConst;
 import com.example.demo.form.LoginForm;
-import com.example.demo.service.LoginService;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -23,15 +20,6 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
-
-	/** ログイン画面 Service */
-	private final LoginService service;
-
-	/** PasswordEncoder */
-	private final PasswordEncoder passwordEncoder;
-
-	/** メッセージソース */
-	private final MessageSource messageSource;
 
 	/** セッション情報 */
 	private final HttpSession session;
