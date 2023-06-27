eac4ab07faafa82df9d21d0fe3deffacc5be378e
shunya <fjfj_07fabric@outlook.jp>
2023-06-27 17:30:48 +0900

    [update] part23時点ソース

--- src/main/java/com/example/demo/authentication/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/authentication/UserDetailsServiceImpl.java
@@ -1,5 +1,10 @@
 package com.example.demo.authentication;
 
+import java.time.LocalDateTime;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +28,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	/** ユーザー情報テーブルRepository */
 	private final UserInfoRepository repository;
 
+	/** アカウントロックを行うログイン失敗回数境界値 */
+	private final int LOCKING_BORDER_COUNT = 3;
+
+	/** アカウントロックの継続時間 */
+	private final int LOCKING_TIME = 1;
+
 	/**
 	 * ユーザー情報生成
 	 * 
@@ -34,10 +45,47 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		var userInfo = repository.findById(username)
 				.orElseThrow(() -> new UsernameNotFoundException(username));
 
+		var accountLockedTime = userInfo.getAccountLockedTime();
+		var isAccountLocked = accountLockedTime != null
+				&& accountLockedTime.plusHours(LOCKING_TIME).isAfter(LocalDateTime.now());
+
 		return User.withUsername(userInfo.getLoginId())
 				.password(userInfo.getPassword())
 				.roles("USER")
+				.disabled(userInfo.isDisabled())
+				.accountLocked(isAccountLocked)
 				.build();
+	}
+
+	/**
+	 * 認証失敗時にログイン失敗回数を加算、ロック日時を更新する
+	 * 
+	 * @param event イベント情報
+	 */
+	@EventListener
+	public void handle(AuthenticationFailureBadCredentialsEvent event) {
+		var loginId = event.getAuthentication().getName();
+		repository.findById(loginId).ifPresent(userInfo -> {
+			repository.save(userInfo.incrementLoginFailureCount());
+
+			var isReachFailureCount = userInfo.getLoginFailureCount() == LOCKING_BORDER_COUNT;
+			if (isReachFailureCount) {
+				repository.save(userInfo.updateAccountLocked());
+			}
+		});
+	}
+
+	/**
+	 * 認証成功時にログイン失敗回数をリセットする
+	 * 
+	 * @param event イベント情報
+	 */
+	@EventListener
+	public void handle(AuthenticationSuccessEvent event) {
+		var loginId = event.getAuthentication().getName();
+		repository.findById(loginId).ifPresent(userInfo -> {
+			repository.save(userInfo.resetLoginFailureInfo());
+		});
 	}
 
 }
--- src/main/java/com/example/demo/entity/UserInfo.java
+++ src/main/java/com/example/demo/entity/UserInfo.java
@@ -1,9 +1,12 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
@@ -15,6 +18,7 @@ import lombok.Data;
 @Entity
 @Table(name = "user_info")
 @Data
+@AllArgsConstructor
 public class UserInfo {
 
 	/** ログインID */
@@ -24,4 +28,47 @@ public class UserInfo {
 
 	/** パスワード */
 	private String password;
+
+	/** ログイン失敗回数 */
+	@Column(name = "login_failure_count")
+	private int loginFailureCount = 0;
+
+	/** アカウントロック日時 */
+	@Column(name = "account_locked_time")
+	private LocalDateTime accountLockedTime;
+
+	/** 利用可能か(true:利用可能) */
+	@Column(name = "is_disabled")
+	private boolean isDisabled;
+
+	public UserInfo() {
+	}
+
+	/**
+	 * ログイン失敗回数をインクリメントする
+	 * 
+	 * @return ログイン失敗回数がインクリメントされたUserInfo
+	 */
+	public UserInfo incrementLoginFailureCount() {
+		return new UserInfo(loginId, password, ++loginFailureCount, accountLockedTime, isDisabled);
+	}
+
+	/**
+	 * ログイン失敗情報をリセットする
+	 * 
+	 * @return ログイン失敗情報がリセットされたUserInfo
+	 */
+	public UserInfo resetLoginFailureInfo() {
+		return new UserInfo(loginId, password, 0, null, isDisabled);
+	}
+
+	/**
+	 * アカウントロック状態に更新する
+	 * 
+	 * @return ログイン失敗階位数、アカウントロック日時が更新されたUserInfo
+	 */
+	public UserInfo updateAccountLocked() {
+		return new UserInfo(loginId, password, 0, LocalDateTime.now(), isDisabled);
+	}
+
 }
