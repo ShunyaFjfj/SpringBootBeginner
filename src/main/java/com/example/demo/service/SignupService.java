2f7bd9d3e0d939ff77bea5f2afec3b768706fb8e
shunya <fjfj_07fabric@outlook.jp>
2023-08-17 12:30:12 +0900

    [add,update] part29時点ソース

--- src/main/java/com/example/demo/service/SignupService.java
+++ src/main/java/com/example/demo/service/SignupService.java
@@ -2,35 +2,16 @@ package com.example.demo.service;
 
 import java.util.Optional;
 
-import org.dozer.Mapper;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.constant.AuthorityKind;
 import com.example.demo.entity.UserInfo;
 import com.example.demo.form.SignupForm;
-import com.example.demo.repository.UserInfoRepository;
-
-import lombok.RequiredArgsConstructor;
 
 /**
- * ユーザー登録画面Serviceクラス
+ * ユーザー登録画面Serviceインターフェース
  * 
  * @author ys-fj
  *
  */
-@Service
-@RequiredArgsConstructor
-public class SignupService {
-
-	/** ユーザー情報テーブルRepositoryクラス */
-	private final UserInfoRepository repository;
-
-	/** Dozer Mapper */
-	private final Mapper mapper;
-
-	/** パスワードエンコーダー */
-	private final PasswordEncoder passwordEncoder;
+public interface SignupService {
 
 	/**
 	 * 画面の入力情報を元にユーザー情報テーブルの新規登録を行います。
@@ -44,17 +25,5 @@ public class SignupService {
 	 * @param form 入力情報
 	 * @return 登録情報(ユーザー情報Entity)、既に同じユーザIDで登録がある場合はEmpty
 	 */
-	public Optional<UserInfo> resistUserInfo(SignupForm form) {
-		var userInfoExistedOpt = repository.findById(form.getLoginId());
-		if (userInfoExistedOpt.isPresent()) {
-			return Optional.empty();
-		}
-
-		var userInfo = mapper.map(form, UserInfo.class);
-		var encodedPassword = passwordEncoder.encode(form.getPassword());
-		userInfo.setPassword(encodedPassword);
-		userInfo.setAuthority(AuthorityKind.ITEM_WATCHER.getAuthorityKind());
-
-		return Optional.of(repository.save(userInfo));
-	}
+	public Optional<UserInfo> resistUserInfo(SignupForm form);
 }
---
+++ src/main/java/com/example/demo/service/SignupServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.dozer.Mapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.constant.AuthorityKind;
+import com.example.demo.entity.UserInfo;
+import com.example.demo.form.SignupForm;
+import com.example.demo.repository.UserInfoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * ユーザー登録画面Service実装クラス
+ * 
+ * @author ys-fj
+ *
+ */
+@Service
+@RequiredArgsConstructor
+public class SignupServiceImpl implements SignupService {
+
+	/** ユーザー情報テーブルRepositoryクラス */
+	private final UserInfoRepository repository;
+
+	/** Dozer Mapper */
+	private final Mapper mapper;
+
+	/** パスワードエンコーダー */
+	private final PasswordEncoder passwordEncoder;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Optional<UserInfo> resistUserInfo(SignupForm form) {
+		var userInfoExistedOpt = repository.findById(form.getLoginId());
+		if (userInfoExistedOpt.isPresent()) {
+			return Optional.empty();
+		}
+
+		var userInfo = mapper.map(form, UserInfo.class);
+		var encodedPassword = passwordEncoder.encode(form.getPassword());
+		userInfo.setPassword(encodedPassword);
+		userInfo.setAuthority(AuthorityKind.ITEM_WATCHER.getAuthorityKind());
+
+		return Optional.of(repository.save(userInfo));
+	}
+}
