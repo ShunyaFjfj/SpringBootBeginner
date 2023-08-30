a8614bb361cf9c10911ace94e0ea763b95a97e1f
shunya <63493199+ShunyaFjfj@users.noreply.github.com>
2023-08-30 18:29:02 +0900

    [fix] part34時点ソース修正
    ユーザー登録時に「利用可能か」の項目がfalseではなくnullで登録されてしまう不具合を修正

--- src/main/java/com/example/demo/service/SignupServiceImpl.java
+++ src/main/java/com/example/demo/service/SignupServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.constant.AuthorityKind;
+import com.example.demo.constant.UserStatusKind;
 import com.example.demo.entity.UserInfo;
 import com.example.demo.form.SignupForm;
 import com.example.demo.repository.UserInfoRepository;
@@ -46,6 +47,7 @@ public class SignupServiceImpl implements SignupService {
 		var userInfo = mapper.map(form, UserInfo.class);
 		var encodedPassword = passwordEncoder.encode(form.getPassword());
 		userInfo.setPassword(encodedPassword);
+		userInfo.setStatus(UserStatusKind.ENABLED);
 		userInfo.setAuthority(AuthorityKind.ITEM_WATCHER);
 		userInfo.setCreateTime(LocalDateTime.now());
 		userInfo.setUpdateTime(LocalDateTime.now());
