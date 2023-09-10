c3db501759696775b0f50ec6b0e1fde4b3b6327b
shunya <63493199+ShunyaFjfj@users.noreply.github.com>
2023-09-10 19:58:49 +0900

    [add,update] part37時点ソース

---
+++ src/main/resources/static/js/userList.js
@@ -0,0 +1,33 @@
+/**
+ * ユーザー一覧画面
+ */
+$(function() {
+	// テーブルの行をクリックしたときの処理
+	$('#userList tbody tr').on('click', function() {
+		// すべての行の選択状態を解除
+		$('#userList tbody tr').removeClass('table-active');
+		// クリックされた行に選択状態のクラスを追加
+		$(this).addClass('table-active');
+		// 更新ボタン、削除ボタンを活性化
+		$('#updateBtn').removeAttr('disabled');
+		$('#deleteDummyBtn').removeAttr('disabled');
+		
+		// ログインID一時保管
+		editSelectedLoginId($(this));
+	});
+});
+
+/**
+ * テーブルで選択された行のログインIDを画面のhidden要素に保管します。
+ * 
+ * @param row 選択された行情報
+ */
+function editSelectedLoginId(row) {
+	row.find('td').each(function() {
+		var columnId = $(this).attr('id');
+		if (columnId.startsWith('loginId_')) {
+			$('#selectedLoginId').val($(this).text());
+			return false;
+		}
+	});
+}
