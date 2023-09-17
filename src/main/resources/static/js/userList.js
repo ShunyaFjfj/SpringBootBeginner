00f69c51ff67e0518c85a7d58bc758309166be83
shunya <63493199+ShunyaFjfj@users.noreply.github.com>
2023-09-17 17:24:16 +0900

    [update] part39(cssクラス名修正)時点ソース

--- src/main/resources/static/js/userList.js
+++ src/main/resources/static/js/userList.js
@@ -5,9 +5,9 @@ $(function() {
 	// テーブルの行をクリックしたときの処理
 	$('#userList tbody tr').on('click', function() {
 		// すべての行の選択状態を解除
-		$('#userList tbody tr').removeClass('table-active');
+		$('#userList tbody tr').removeClass('table-row-active');
 		// クリックされた行に選択状態のクラスを追加
-		$(this).addClass('table-active');
+		$(this).addClass('table-row-active');
 		// 更新ボタン、削除ボタンを活性化
 		$('#updateBtn').removeAttr('disabled');
 		$('#deleteDummyBtn').removeAttr('disabled');
